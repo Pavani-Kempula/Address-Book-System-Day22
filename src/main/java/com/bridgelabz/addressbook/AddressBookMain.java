1da6d15b3290c9252a9f057c5231e3b0fe1e58f3
PavaniKempula <pavanikempula1998@gmail.com>
2021-10-19 19:58:12 +0530

    ADDED: Welcome to Address Book System

---
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        //Displaying Welcome Message
+        System.out.println("Welcome to Address Book Problem");
+    }
+}
+
