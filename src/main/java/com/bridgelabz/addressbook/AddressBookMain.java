1ffa314d6d1c316a69735b5174e2306a422f609e
PavaniKempula <pavanikempula1998@gmail.com>
2021-10-24 01:02:38 +0530

    ADDED : ArrayList of Multiple Contacts

--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -9,36 +9,7 @@ public class AddressBookMain {
 
         Scanner sc = new Scanner(System.in);
         AddressBook addressBook = new AddressBook();
-
-        while (true){
-
-            System.out.println("Enter \n 1 . Add new contact \n 2 . Edit \n 3 . Delete \n 0 . for exit");
-            int op = sc.nextInt();
-            sc.nextLine();
-            switch (op) {
-                case 1:
-                    addressBook.add();
-                    System.out.println("You have Entered following data");
-                    System.out.println(addressBook.contact);
-                    break;
-                case 2:
-                    addressBook.editContact();
-                    System.out.println("You have Entered following data");
-                    System.out.println(addressBook.contact);
-                    System.out.println("Thank for Using Address book");
-                    break;
-                case 3:
-                    addressBook.delete();
-                    System.out.println("Now data is :" + addressBook.contact);
-                    break;
-                case 0:
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("You entered the wrong please input choose correct one : ");
-                    continue;
-            }
-        }
+        addressBook.getMenu();
     }
 }
 
