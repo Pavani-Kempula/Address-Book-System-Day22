d9dfe0dfa80eb4f11a01db1e983c57789b7489ff
PavaniKempula <pavanikempula1998@gmail.com>
2021-11-10 20:16:50 +0530

    Added: sorting contacts by persone name.

--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -59,7 +59,7 @@ public class AddressBookMain {
                     addressBook.viewByOption(addressBookMap);
                     break;
                 case 6:
-                    System.out.println("Welcome to the couter");
+                    System.out.println("Welcome to the counter");
                     addressBook.countByOption();
                     break;
                 case 7:
